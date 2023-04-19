e278a1167f9aabb79df15d3f3885d7c7a5099418
Tirasz <zacsi143@gmail.com>
2023-04-19 15:23:50 +0200

    Added linux sh project run script

--- run_project.sh
+++ run_project.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# Extend classpath with antlr.jar
-CLASSPATH=.:$(dirname "$0")/antlr-4.11.1-complete.jar:$CLASSPATH
 
 echo "## Generating java files from grammar..."
-java -jar $(dirname "$0")/antlr-4.11.1-complete.jar Byte.g4
+java -cp .:antlr-4.11.1-complete.jar org.antlr.v4.Tool Byte.g4
 
 echo "## Compiling generated java files..."
-javac Byte*.java
+javac -cp .:antlr-4.11.1-complete.jar *.java
 
 echo "## Running the parser on input..."
-java ByteParser input.txt
+java -cp .:antlr-4.11.1-complete.jar ByteParser input.txt
 
 echo "## Running the VM on the output..."
-python virtual_machine.py output.bytecode
+python3 virtual_machine.py output.bytecode
